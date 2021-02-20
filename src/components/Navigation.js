f5f3885ef5eea027c0a5d945b1d4a7a7195dd9a1
Jae Wu <jaewuchun@gmail.com>
2021-02-19 21:01:37 -0800

    minor navigation tweaks

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, NavLink, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, NavLink, Link, Redirect, useHistory } from 'react-router-dom';
+import { AuthProvider, useFirebaseAuth } from '../contexts/AuthContextFirebase';
 
 const Navigation = () => {
+    const { currentUser, logout } = useFirebaseAuth();
+
+    const history = useHistory();
+
+    async function handleLogout(e) {
+        e.preventDefault();
+
+        try {
+            await logout();
+            history.push('/');
+        } catch {
+            console.log('Failed to log out.');
+        }
+    }
 
     return (
         
@@ -15,8 +30,19 @@ const Navigation = () => {
                 <div className='navigation'>
                     <NavLink className='nav-link' activeClassName='active' to="demo">View Demo</NavLink>
                     <NavLink className='nav-link' to="support">Support</NavLink>
-                    <NavLink className='nav-link member' to="login">Login</NavLink>
-                    <NavLink className='nav-link member' to="register">Register</NavLink>
+                    {(currentUser) ? (
+                        // need further splitting based on role (staff vs secretariat)
+                        <>
+                            <NavLink className='nav-link member colour' to='chair'>Chair</NavLink>
+                            <NavLink className='nav-link member colour' to='dashboard'>Dashboard</NavLink>
+                            <button className='nav-link member' onClick={handleLogout}>Log Out</button>
+                        </>
+                    ) : (
+                        <>
+                            <NavLink className='nav-link member' to="login">Login</NavLink>
+                            <NavLink className='nav-link member' to="register">Register</NavLink>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
