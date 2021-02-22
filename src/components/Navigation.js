9832b2f9d30b8e9bd9ff2828b58112e02aca734c
Jae Wu <jaewuchun@gmail.com>
2021-02-21 18:10:46 -0800

    fixed an error

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -25,9 +25,9 @@ const Navigation = () => {
                     </Link>
                 </div>
                 <div className='navigation'>
-                    {(currentUser) ? '' : (
-                        // <NavLink className='nav-link' activeClassName='active' to="demo">View Demo</NavLink>
-                    )}
+                    {/* {(currentUser) ? '' : (
+                        <NavLink className='nav-link' activeClassName='active' to="demo">View Demo</NavLink>
+                    )} */}
                     {(currentUser) ? (
                         // need further splitting based on role (staff vs secretariat)
                         <>
