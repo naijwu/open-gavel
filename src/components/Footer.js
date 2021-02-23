f4182a67dd1f6b4c04a64f8a4cd647b20f4568a4
Jae Wu <jaewuchun@gmail.com>
2021-02-22 19:26:51 -0800

    minor footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footer'>
-            the OpenGavel project &middot; free chairing program for MUN<br />
-            want to improve? (need UI help) &middot; <a href='mailto:jaewuchun@gmail.com'>shoot me! (email)</a><br />
+            OpenGavel &middot; free chairing program for MUN<br />
+            want to help improve? &middot; <a href='mailto:jaewuchun@gmail.com'>shoot me! (email)</a><br />
             github &middot; <a href='http://github.com/naijwu/open-gavel'>open-gavel</a> &middot; <a href='http://github.com/naijwu/open-gavel-server'>open-gavel-server</a>
         </div>
     );
