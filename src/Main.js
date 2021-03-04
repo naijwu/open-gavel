6e211971bc591c069c6564d67e1675a814f26d9b
Jae Wu <jaewuchun@gmail.com>
2021-03-03 18:58:31 -0800

    minor css changes

--- src/Main.js
+++ src/Main.js
@@ -11,9 +11,8 @@ const Main = () => {
             <Navigation />
             <div className='main'>
                 <div className='impact'>
-                    <h1>Simple.</h1>
-                    <h1>Powerful.</h1>
-                    <h1>Open.</h1>
+                    <h1>Simple. Powerful. Open.</h1>
+                    <h2>Chairing Program for Model UN Conferences</h2>
                 </div>
                 <div className='features'>
                     <h2>Features</h2>
@@ -49,9 +48,9 @@ const Main = () => {
                             </p>
                         </div>
                         <div className='grid-item'>
-                            <h4>Free and Open &mdash; Forever!</h4>
+                            <h4>Open and (mostly) free!</h4>
                             <p>
-                                OpenGavel will always be free to use for MUNs. All the code is also public on GitHub.
+                                The first 10 committee accounts will always be free. Promise. All the code is also public on GitHub.
                             </p>
                         </div>
                     </div>
