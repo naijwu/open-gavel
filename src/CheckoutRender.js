3b5961824b40fe3a02836d1281eec27699efa84c
bevannutakor <bevannutakor@icloud.com>
2021-03-23 20:33:38 -0700

    minor edits
    Need to figure out env situation

--- src/CheckoutRender.js
+++ src/CheckoutRender.js
@@ -5,7 +5,8 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm"
 import Navigation from "./components/Navigation.js";
 
-const promise = loadStripe("pk_test_51G6WxqGdNKtofFw2p4pKuxr7ZnME1vOuKp3YoqxfTcfZW344gNLt3oHoloZWWonRHhCaftpSlYCD2UPSRl7ihBqb00trYkfCZL")
+//ENV does not work 
+const promise = loadStripe("process.env.PUBLIC_STRIPE_KEY")
 
 export default function CheckoutRender(){
     return(
